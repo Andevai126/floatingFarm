0949d88d11c4767f88f7ba81edf7c971f4381219
Andevai126 <sepvanderbiezen@gmail.com>
2023-11-24 12:25:32 +0100

    fixed number of tabs and added default date and time

--- client/src/components/FarmerCard.vue
+++ client/src/components/FarmerCard.vue
@@ -1,81 +1,87 @@
 <template>
-    <p>This is the FarmerCard component</p>
+  <p>This is the FarmerCard component</p>
 
-    <div style="display: inline-block;">
-      <p style="float: left;">Gras</p>
-      <input type="number" v-model="kilosGras" style="float: left;">
-      <p style="float: left">kilo's</p>
-    </div>
+  <div style="display: inline-block;">
+    <p style="float: left;">Gras</p>
+    <input type="number" v-model="kilosGras" style="float: left;">
+    <p style="float: left">kilo's</p>
+  </div>
 
-    <br>
+  <br>
 
-    <div style="display: inline-block;">
-      <p style="float: left;">Bierbostel</p>
-      <input type="number" v-model="kilosBierbostel" style="float: left;">
-      <p style="float: left">kilo's</p>
-    </div>
+  <div style="display: inline-block;">
+    <p style="float: left;">Bierbostel</p>
+    <input type="number" v-model="kilosBierbostel" style="float: left;">
+    <p style="float: left">kilo's</p>
+  </div>
 
-    <br>
+  <br>
 
-    <div style="display: inline-block;">
-      <p style="float: left;">DDGS Proticorn</p>
-      <input type="number" v-model="kilosDDGSProticorn" style="float: left;">
-      <p style="float: left">kilo's</p>
-    </div>
+  <div style="display: inline-block;">
+    <p style="float: left;">DDGS Proticorn</p>
+    <input type="number" v-model="kilosDDGSProticorn" style="float: left;">
+    <p style="float: left">kilo's</p>
+  </div>
 
-    <br>
-    
-    <div style="display: inline-block;">
-      <p style="float: left;">Sinaasappelschillen</p>
-      <input type="number" v-model="kilosSinaasappelschillen" style="float: left;">
-      <p style="float: left">kilo's</p>
-    </div>
-
-    <br>
-
-    <div v-for="(product, index) in ExtraProductsInMix" :key="index">
-      <div style="width: 500px; margin-left: auto; margin-right: auto; border: black solid 2px">
-
-        <div style="display: inline-block; float: center">
-          <p style="float: left;">Product</p>
-          <input type="text" v-model="product.name" placeholder="Enter data" style="float: left;">
-        </div>
-
-        <br>
-        
-        <div style="display: inline-block; float: center">
-          <p style="float: left;">Hoeveelheid</p>
-          <input type="number" v-model="product.quantity" style="float: left;">
-          <p style="float: left;">kilo</p>
-        </div>
-
-      </div>
-      <br>
-    </div>
-
-    <br>
-
-    <button @click="addProduct">Voeg product toe...</button>
-
-    <br>
-
-    <div style="display: inline-block;">
-      <input type="date" v-model="date" style="float: left">
-      <input type="time" v-model="time" style="float: left">
-    </div>
-
-    <br>
-
-    <input type="text" placeholder="Notes..." v-model="notes" style="height: 100px;">
-
-    <br>
-
-    <button @click="saveMix()">Sla mix op ;) !</button>
-    <br>
-    <img alt="Farmer design" src="./../assets/Farmer.png">
-  </template>
+  <br>
   
-  <script>
+  <div style="display: inline-block;">
+    <p style="float: left;">Sinaasappelschillen</p>
+    <input type="number" v-model="kilosSinaasappelschillen" style="float: left;">
+    <p style="float: left">kilo's</p>
+  </div>
+
+  <br>
+
+  <div v-for="(product, index) in ExtraProductsInMix" :key="index">
+    <div style="width: 500px; margin-left: auto; margin-right: auto; border: black solid 2px">
+
+      <div style="display: inline-block; float: center">
+        <p style="float: left;">Product</p>
+        <input type="text" v-model="product.name" placeholder="Enter data" style="float: left;">
+      </div>
+
+      <br>
+      
+      <div style="display: inline-block; float: center">
+        <p style="float: left;">Hoeveelheid</p>
+        <input type="number" v-model="product.quantity" style="float: left;">
+        <p style="float: left;">kilo</p>
+      </div>
+
+    </div>
+    <br>
+  </div>
+
+  <br>
+
+  <button @click="addProduct">Voeg product toe...</button>
+
+  <br>
+
+  <div style="display: inline-block;">
+    <input type="date" v-model="date" style="float: left">
+    <input type="time" v-model="time" style="float: left">
+  </div>
+
+  <br>
+
+  <input type="text" placeholder="Notes..." v-model="notes" style="height: 100px;">
+
+  <br>
+
+  <button @click="saveMix()">Sla mix op ;) !</button>
+  <br>
+  <img alt="Farmer design" src="./../assets/Farmer.png">
+</template>
+  
+<script>
+  import { ref } from 'vue';
+
+  var currentDate = ref('');
+  var currentTime = ref('');
+
+
   export default {
     name: 'FarmerCard',
     methods: {
@@ -100,6 +106,11 @@
         //reset variables
       }
     },
+    setup() {
+      const newDate = new Date();
+      currentDate.value = newDate.toISOString().slice(0, 10);
+      currentTime.value = newDate.toTimeString().slice(0, 5);
+    },
     data() {
       return {
         kilosGras: 600,
@@ -107,16 +118,16 @@
         kilosDDGSProticorn: 300,
         kilosSinaasappelschillen: 0,
         ExtraProductsInMix: [],
-        date: null,
-        time: null,
+        date: currentDate,
+        time: currentTime,
         notes: ''
       }
     }
   }
-  </script>
+</script>
   
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  
-  </style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
   
