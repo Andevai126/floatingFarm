bd6249f9b668566520aa10b627bf012a8069a879
Andevai126 <sepvanderbiezen@gmail.com>
2023-12-06 11:10:41 +0100

    added bootstrap package

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { initializeMsal } from './msalConfig';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 const app = createApp(App);
 app.mount('#app');
 
