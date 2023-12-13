25d5d881923eee5bd396aea94614c9db844f7cdb
Andevai126 <sepvanderbiezen@gmail.com>
2023-12-13 12:39:29 +0100

    change indentation

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,12 @@
 import { ref } from 'vue'
 
 export const store = {
-    msalInstance: null,
-    accountId: "",
-    authenticated: ref(false),
-    accessToken: "",
-    username: ref(""),
+  msalInstance: null,
+  accountId: "",
+  authenticated: ref(false),
+  accessToken: "",
+  username: ref(""),
 
-    roleId: ref(-1),
-    roleTitle: ref("")
+  roleId: ref(-1),
+  roleTitle: ref("")
 };
