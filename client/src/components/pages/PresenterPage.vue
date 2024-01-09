15122837a5c6119f377a71061d7a72af09742377
Andevai126 <sepvanderbiezen@gmail.com>
2024-01-09 08:46:29 +0100

    improved presenter page design again

--- client/src/components/pages/PresenterPage.vue
+++ client/src/components/pages/PresenterPage.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container rounded border bg-light shadow p-3 mt-5 mb-3">
-		<img alt="Presenter design" src="../../assets/Presenter.png">
+		<img alt="Presenter design" src="../../assets/Presenter.png" style="width: 90%;">
 	</div>
 </template>
   
