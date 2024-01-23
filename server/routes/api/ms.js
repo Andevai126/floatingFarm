cd412c1cf56b75d76159a2cb1874e0ee906e25a3
Andevai126 <sepvanderbiezen@gmail.com>
2024-01-23 19:56:32 +0100

    added second version of ms getMixes

--- server/routes/api/ms.js
+++ server/routes/api/ms.js
@@ -31,4 +31,53 @@ router.post('/getMixesV1', (req, res) => {
     }
 });
 
+// Get all mixes with VEM and DVE - version two
+router.post('/getMixesV2', (req, res) => {
+    if (req.body.key === process.env.API_KEY){
+        query(`
+            SELECT pim.mixID, m.dateTime, pim.productID, p.name, pim.kilos, pim.kilos*p.vemPerKilo AS vem, pim.kilos*p.dvePerKilo AS dve
+            FROM productsinmix AS pim
+            LEFT JOIN mixes AS m ON pim.mixID = m.ID
+            LEFT JOIN products AS p ON pim.productID = p.ID;`,
+        [], (results, fields) => {
+            const condensedData = results.reduce((acc, current) => {
+                const existingMix = acc.find(item => item.mixID === current.mixID);
+            
+                if (existingMix) {
+                    // MixID already exists, update the existing mix
+                    existingMix.nutrients.vemTotal += current.vem;
+                    existingMix.nutrients.dveTotal += current.dve;
+
+                    existingMix.products.push({
+                        productID: current.productID,
+                        name: current.name,
+                        kilos: current.kilos
+                    });
+                } else {
+                    // MixID doesn't exist, create a new mix
+                    acc.push({
+                        mixID: current.mixID,
+                        dateTime: Math.floor(new Date(current.dateTime).getTime() / 1000),
+                        nutrients: {
+                            vemTotal: current.vem,
+                            dveTotal: current.dve,
+                        },
+                        products: [
+                            {
+                                productID: current.productID,
+                                name: current.name,
+                                kilos: current.kilos
+                            }
+                        ]
+                    });
+                }
+                return acc;
+            }, []);
+            res.status(200).send(condensedData);
+        });
+    } else {
+        res.status(401).send();
+    }
+});
+
 module.exports = router;
