7c5241f64296a4d83b2dcf62eb542625572cd3f1
Andevai126 <sepvanderbiezen@gmail.com>
2024-01-26 15:43:30 +0100

    start of more accurate vem graph

--- client/src/components/cards/graphs/VemGraph.vue
+++ client/src/components/cards/graphs/VemGraph.vue
@@ -57,11 +57,34 @@
 					allDates.push(currentDate.toISOString().slice(0, 10));
 				}
 
+				// Generate guestimated filler data for milk produced with a bit of manually added real data
+				var milkProduced = [...Array(allDates.length).fill(22)];
+				var startDate = allDates.indexOf("2024-01-11");
+				var knownMilkProduced = [21.40, 19.89, 19.70, 19.67, 19.49, 19.21, 19.96, 20.21, 21.36, 21.66, 21.73, 21.53, 21.87, 21.87, 21.90, 22.07];
+				for (let i = 0; i < knownMilkProduced.length; i++) {
+					milkProduced[startDate+i] = knownMilkProduced[i];
+				}
+
+				// Retrieve static theoretical data of two values
+				var vemNeeded = [...Array(allDates.length).fill(0)];
+				for (let i = 0; i < vemNeeded.length; i++) {
+					vemNeeded[i] = 16567;
+					if (milkProduced[i] >= 18 && milkProduced[i] < 20) {
+						vemNeeded[i] = 14890;
+					}
+					if (milkProduced[i] >= 20 && milkProduced[i] < 22) {
+						vemNeeded[i] = 15729;
+					}
+					if (milkProduced[i] >= 22 && milkProduced[i] < 24) {
+						vemNeeded[i] = 16567;
+					}
+				}
+
 				// Generate an array with at the correct indices the available vem values
-				var vemData = Array(allDates.length).fill(null);
+				var vemProvided = Array(allDates.length).fill(null);
 				for (let i = 0; i < nutrientsOfMixes.length; i++) {
 					var index = allDates.indexOf(new Date(nutrientsOfMixes[i].dateTime).toISOString().slice(0, 10));
-					vemData[index] = nutrientsOfMixes[i].vemTotal;
+					vemProvided[index] = nutrientsOfMixes[i].vemTotal;
 				}
 
 				defaultChartData.value = {
@@ -69,7 +92,7 @@
 					datasets: [
 						{
 							label: "Average milk production",
-							data: [...Array(allDates.length).fill(22)],
+							data: milkProduced,
 							yAxisID: "A",
 							borderColor: '#bdbdbd',
 							backgroundColor: '#ffffff',
@@ -80,7 +103,7 @@
 						{
 							type: 'line',
 							label: "Average VEM needed",
-							data: [...Array(allDates.length).fill(16567)],
+							data: vemNeeded,
 							yAxisID: "B",
 							borderColor: "#00ff00",
 							backgroundColor: "#00bd00",
@@ -90,7 +113,7 @@
 						{
 							type: 'line',
 							label: "Average VEM provided",
-							data: vemData,
+							data: vemProvided,
 							yAxisID: "B",
 							borderColor: "#0000ff",
 							backgroundColor: "#0000bd",
